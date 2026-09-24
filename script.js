2ab24e78aa55397d388bef1ce33727e809b1d6c8
isatvsc <isabellyteixeiravasconcelos@gmail.com>
2026-09-24 11:05:42 -0300

    feat: biblioteca owl carousel adicionada, pagina blogNu estilizada

--- script.js
+++ script.js
@@ -127,3 +127,24 @@ function home4CardSelector(){
 
     
 }
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:7000,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
